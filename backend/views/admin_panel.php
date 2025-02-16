ec67a379e0c85d35a50cad5a91b931b0ef0b3cb3
shahzodkenjayev <shahzodkenjayevyangiboyevich@gmail.com>
2025-02-16 20:45:33 +0500

    O'zgarishlar qo'shildi

--- backend/views/admin_panel.php
+++ backend/views/admin_panel.php
@@ -3,29 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ikki Tugma</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            text-align: center;
-            margin-top: 50px;
-        }
-        .btn {
-            display: inline-block;
-            padding: 10px 20px;
-            font-size: 16px;
-            margin: 10px;
-            cursor: pointer;
-            border: none;
-            border-radius: 5px;
-            color: #fff;
-            background-color: #007bff;
-            text-decoration: none;
-        }
-        .btn:hover {
-            background-color: #0056b3;
-        }
-    </style>
+    <title>Admin panel</title>
+    <link rel="stylesheet" href="css/style.css"> <!-- CSS faylini ulash -->
+    
 </head>
 <body>
 
